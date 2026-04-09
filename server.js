79a7020869aa1c8efeb8fa64e7a08ff792e1a89a
AkshatShah1511 <shahakshat1511@gmail.com>
2026-04-09 15:02:54 +0530

    fix server.js

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ process.on('uncaughtException', (err) => {
   console.error('Uncaught Exception:', err)
   process.exit(1)
 })
+
 require("dotenv").config();
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
 const app = require("./app");
 
 const PORT = process.env.PORT || 3000;
